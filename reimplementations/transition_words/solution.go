7da1b9d526d8e55eafe06649e9a3a375782e1085
alee <anlee.gen@gmail.com>
2023-06-08 18:04:04 -0700

    Add transition words solution + test (forgot to add these to prior commit)

--- reimplementations/transition_words/solution.go
+++ reimplementations/transition_words/solution.go
@@ -1,1 +1,77 @@
-package main
+package transition_words
+
+/*
+Given startWord, endWord: is it possible to transition startWord to endWord one letter at a time?
+Each transition can only be 1 letter swap, and each transition word must be a valid word.
+
+eg) startWord = "cat", endWord = "dog"
+	cat -> hat -> hot -> hog -> dog
+	cat -> cot -> cog -> dog
+	cat -> pat -> pot -> dot -> dog
+*/
+
+type partialDict struct {
+	wordSet map[string]bool
+}
+
+func NewPartialDict(wordList []string) *partialDict {
+	wordSet := make(map[string]bool)
+	for _, w := range wordList {
+		wordSet[w] = true
+	}
+	return &partialDict{wordSet: wordSet}
+}
+
+func (d *partialDict) IsValidWord(word string) bool {
+	return d.wordSet[word]
+}
+
+func TransitionWords(startWord string, endWord string, wordList []string) int {
+	if startWord == endWord {
+		return 0
+	}
+
+	dict := NewPartialDict(wordList)
+	if !dict.IsValidWord(startWord) || !dict.IsValidWord(endWord) {
+		return -1
+	}
+
+	beginSet := map[string]bool{startWord: true}
+	endSet := map[string]bool{endWord: true}
+	visited := make(map[string]bool)
+	level := 1
+	for len(beginSet) > 0 && len(endSet) > 0 {
+		if len(beginSet) > len(endSet) {
+			beginSet, endSet = endSet, beginSet // swap to always extend the smaller set
+		}
+
+		nextLevelSet := make(map[string]bool)
+
+		for word := range beginSet {
+			for j := 0; j < len(word); j++ {
+				for c := 'a'; c <= 'z'; c++ {
+					newWord := word[:j] + string(c) + word[j+1:]
+					if endSet[newWord] {
+						//fmt.Printf(
+						//	"[LEVEL=%d]\n    BeginSet: %v\n    EndSet: %v\n    NextLevelSet: %v\n",
+						//	level, beginSet, endSet, nextLevelSet,
+						//)
+						return level + 1 // reached the endWord, return the level
+					}
+					if dict.IsValidWord(newWord) && !visited[newWord] {
+						nextLevelSet[newWord] = true // enqueue the valid transition word
+						visited[newWord] = true      // mark the word as visited
+					}
+				}
+			}
+		}
+		//fmt.Printf(
+		//	"[LEVEL=%d]\n    BeginSet: %v\n    EndSet: %v\n    NextLevelSet: %v\n",
+		//	level, beginSet, endSet, nextLevelSet,
+		//)
+		beginSet = nextLevelSet
+		level++
+	}
+
+	return -1 // unable to reach the endWord
+}
--- reimplementations/transition_words/solution_test.go
+++ reimplementations/transition_words/solution_test.go
@@ -1,1 +1,110 @@
-package transition_words
+package transition_words_test
+
+import (
+	"go-dsa/reimplementations/transition_words"
+	"testing"
+)
+
+func TestTransitionWords(t *testing.T) {
+	startWord := "cat"
+	endWord := "dog"
+	wordList := []string{"cat", "hat", "hot", "hog", "dog", "cot", "cog", "pat", "pot", "dot"}
+
+	result := transition_words.TransitionWords(startWord, endWord, wordList)
+	expected := 4
+
+	if result != expected {
+		t.Errorf("Expected ladder length: %d, but got: %d", expected, result)
+	}
+}
+
+func TestTransitionWords_NoSolution(t *testing.T) {
+	startWord := "cat"
+	endWord := "dog"
+	wordList := []string{"cat", "hat", "hot", "hog", "pot", "dot"}
+
+	result := transition_words.TransitionWords(startWord, endWord, wordList)
+	expected := -1
+
+	if result != expected {
+		t.Errorf("Expected ladder length: %d, but got: %d", expected, result)
+	}
+}
+
+func TestTransitionWords_StartAndEndSame(t *testing.T) {
+	startWord := "cat"
+	endWord := "cat"
+	wordList := []string{"cat", "hat", "hot", "hog", "dog", "cot", "cog", "pat", "pot", "dot"}
+
+	result := transition_words.TransitionWords(startWord, endWord, wordList)
+	expected := 0
+
+	if result != expected {
+		t.Errorf("Expected ladder length: %d, but got: %d", expected, result)
+	}
+}
+
+func TestTransitionWords_EndWordNotInList(t *testing.T) {
+	startWord := "cat"
+	endWord := "dog"
+	wordList := []string{"cat", "hat", "hot", "hog", "pot", "dot"}
+
+	result := transition_words.TransitionWords(startWord, endWord, wordList)
+	expected := -1
+
+	if result != expected {
+		t.Errorf("Expected ladder length: %d, but got: %d", expected, result)
+	}
+}
+
+func TestTransitionWords_EmptyWordList(t *testing.T) {
+	startWord := "cat"
+	endWord := "dog"
+	var wordList []string
+
+	result := transition_words.TransitionWords(startWord, endWord, wordList)
+	expected := -1
+
+	if result != expected {
+		t.Errorf("Expected ladder length: %d, but got: %d", expected, result)
+	}
+}
+
+func TestTransitionWords_NoTransitionNeeded(t *testing.T) {
+	startWord := "cat"
+	endWord := "cat"
+	wordList := []string{"cat", "hat", "hot", "hog", "dog"}
+
+	result := transition_words.TransitionWords(startWord, endWord, wordList)
+	expected := 0
+
+	if result != expected {
+		t.Errorf("Expected ladder length: %d, but got: %d", expected, result)
+	}
+}
+
+func TestTransitionWords_SingleTransition(t *testing.T) {
+	startWord := "cat"
+	endWord := "dog"
+	wordList := []string{"cat", "cot", "cog", "dog"}
+
+	result := transition_words.TransitionWords(startWord, endWord, wordList)
+	expected := 4
+
+	if result != expected {
+		t.Errorf("Expected ladder length: %d, but got: %d", expected, result)
+	}
+}
+
+func TestTransitionWords_LongerTransitions(t *testing.T) {
+	startWord := "hit"
+	endWord := "cog"
+	wordList := []string{"hit", "hot", "dot", "dog", "lot", "log", "cog"}
+
+	result := transition_words.TransitionWords(startWord, endWord, wordList)
+	expected := 5
+
+	if result != expected {
+		t.Errorf("Expected ladder length: %d, but got: %d", expected, result)
+	}
+}
